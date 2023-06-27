2adf01effb30c76e6906d19cf6fe2531e198a779
Matheus Aguiar <matheus.pit@gmail.com>
2023-06-27 14:58:58 -0300

    remove unrelated tests

--- test/libsolidity/syntaxTests/specialFunctions/abidecode/abi_decode_invalid_argument_ternary_expression_runtimeCode_keyword.sol
+++
@@ -1,9 +0,0 @@
-contract D { }
-
-contract C {
-    function f(bool c) pure public {
-        abi.decode("", ((c ? type(C) : type(D)).runtimeCode));
-    }
-}
-// ----
-// TypeError 1039: (90-125): Argument has to be a type name.
--- test/libsolidity/syntaxTests/specialFunctions/abidecode/abid_decode_invalid_argument_function.sol
+++
@@ -1,7 +0,0 @@
-contract C {
-    function f() pure public {
-        abi.decode("", ((true ? addmod : addmod)(1, 2, 3)));
-    }
-}
-// ----
-// TypeError 1039: (68-101): Argument has to be a type name.
